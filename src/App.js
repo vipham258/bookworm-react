b858cd6c077a899cdf782c2a0b369abf68fd20d8
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-03-15 12:01:22 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+import { HomePage } from "./components/pages/HomePage";
+
+const App=()=> (
+<div>
+  <Route path="/" exact component={HomePage}/>
+</div>
+  );
+ 
+
+export default App;
