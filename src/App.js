3417f5b58ca674877faa34cf248716f1ddbfd132
Vi Pham <vipham25896@gmail.com>
2019-03-24 13:26:45 -0400

    added prettier and gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import { HomePage } from "./components/pages/HomePage";
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { HomePage } from './components/pages/HomePage'
 
-const App=()=> (
-<div>
-  <Route path="/" exact component={HomePage}/>
-</div>
-  );
- 
+const App = () => (
+  <div>
+    <Route path="/" exact component={HomePage} />
+  </div>
+)
 
-export default App;
+export default App
